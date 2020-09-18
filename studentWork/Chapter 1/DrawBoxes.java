b5061084deea04f42f2bc06a532106d8ea3595d0
Mental-Overload/EphrataTEALS <wesley-h-ephrata-teals@outlook.com>
2020-09-18 19:14:08 +0000

    Pika

---
+++ studentWork/Chapter 1/DrawBoxes.java
@@ -0,0 +1,14 @@
+package studentWork.Chapter1;
+
+public class DrawBoxes {
+    
+public static void public static void main(String[] args) {
+   
+    System.out.println("+------+");
+    System.out.println("|      |");
+    System.out.println("|      |");
+    System.out.println("+------+");
+
+}
+
+}
---
+++ studentWork/Unit1/PikachuChatter.java
@@ -0,0 +1,19 @@
+package studentWork.Unit1;
+
+public class PikachuChatter {
+  
+   public static void main(final String[] args) {
+       
+	System.out.println("(pika pika");
+   drawPikachu();
+   System.out.println("piiikaa chhuuuuuuu");
+   drawPikachu();
+   }
+
+public static void drawPikachu () {
+   System.out.println("(\\__//)");
+   System.out.println("(o^.^)");
+   System.out.println("z(_(\")(\")");
+}
+
+}
