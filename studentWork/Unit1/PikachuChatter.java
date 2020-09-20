48735d5fa8b8145f0640584cd00e1f82e08536d2
Mental-Overload/EphrataTEALS <wesley-h-ephrata-teals@outlook.com>
2020-09-20 16:33:41 +0000

    pikachu challenge

--- studentWork/Unit1/PikachuChatter.java
+++ studentWork/Unit1/PikachuChatter.java
@@ -4,14 +4,14 @@ public class PikachuChatter {
   
    public static void main(final String[] args) {
        
-	System.out.println("(pika pika");
+	System.out.println("pika pika");
    drawPikachu();
    System.out.println("piiikaa chhuuuuuuu");
    drawPikachu();
    }
 
 public static void drawPikachu () {
-   System.out.println("(\\__//)");
+   System.out.println("(\\__/)");
    System.out.println("(o^.^)");
    System.out.println("z(_(\")(\")");
 }
